feb59ebfe9c055e1848edbe39fe708add906e345
kjash07 <54998463+kjash07@users.noreply.github.com>
2019-09-07 14:12:57 +0530

    removed credentials

--- list_s3_buckets.py
+++ list_s3_buckets.py
@@ -1,7 +1,5 @@
 import boto3
 
-#s3_obj = boto3.resource('s3', aws_access_key_id="AKIAI4D2JRIXDDSXJXAQ", aws_secret_access_key="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")
-
 s3_obj = boto3.resource('s3')
 
 for each_object in s3_obj.buckets.all():
